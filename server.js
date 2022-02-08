366c4f145d8910961bf22b0529de4943bddd94a9
Purna Chandra Sanapala <purnachandra@Purnas-MacBook-Pro.local>
2022-02-07 21:22:07 -0500

    Added user unique id

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ try{
         console.log("new_vals : ",new_vals)
         var sess = await collection.updateOne(myquery,new_vals)
         console.log("sess : ",sess)
-        res.json({"session_id" : token})
+        res.json({"session_id" : token,"USER_UNIQUE_ID" : opx._id})
     }else{
         res.json({"STATUS" : "ERROR IN LOGING IN"})
     }
@@ -159,7 +159,7 @@ app.post('/login_auth/signup', jsonParser, async function(req,res){
               .catch(error => {
                 console.error(error)
               })
-            res.json({"session_id" : token})
+            res.json({"session_id" : token,"USER_UNIQUE_ID": opx._id})
         }else{
             res.json({"STATUS" : "ERROR SIGNING UP"})
         }
